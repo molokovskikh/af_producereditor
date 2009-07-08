afa719d606d41dc6ce3b8fa0e3f4b2b95905afa2
kvasov <kvasov@55eaebc9-43d5-4fe0-8998-b6831c4e7c87>
2009-07-08 07:29:59 +0000

    Добавил все что не сложилось в первый раз

---
+++ src/ProducerEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProducerEditor
+{
+	public static class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Initialezer.Initialize();
+			Application.Run(new MainForm());
+		}
+	}
+}
