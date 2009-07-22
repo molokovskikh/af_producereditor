b81a1de67f4c5d250ad9d474402bef49409580f3
kvasov <kvasov@55eaebc9-43d5-4fe0-8998-b6831c4e7c87>
2009-07-22 08:32:30 +0000

    Проверка на наличие производителя при переименовании

--- src/ProducerEditor/Views/RenameView.cs
+++ src/ProducerEditor/Views/RenameView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Windows.Forms;
 using ProducerEditor.Models;
 
@@ -9,27 +10,33 @@ namespace ProducerEditor.Views
 		public RenameView(Controller controller, Producer producer)
 		{
 			var errorProvider = new ErrorProvider();
-			var newName = new TextBox
-			              	{
-			              		Text = producer.Name,
-			              		Width = 200,
-			              	};
+			var newName = new TextBox {
+				Text = producer.Name,
+				Width = 200,
+			};
 			table.Controls.Add(newName, 0, 0);
 			Text = "Переименование производителя";
-			Closing += (sender, args) =>
-			           {
-			           	if (DialogResult == DialogResult.Cancel)
-			           		return;
-			           	if (String.IsNullOrEmpty(newName.Text.Trim()))
-			           	{
-			           		errorProvider.SetError(newName, "Название производителя не может быть пустым");
-			           		errorProvider.SetIconAlignment(newName, ErrorIconAlignment.MiddleRight);
-			           		args.Cancel = true;
-			           		return;
-			           	}
-			           	producer.Name = newName.Text;
-			           	controller.Update(producer);
-			           };
+			Closing += (sender, args) => {
+				if (DialogResult == DialogResult.Cancel)
+					return;
+				if (String.IsNullOrEmpty(newName.Text.Trim()))
+				{
+					errorProvider.SetError(newName, "Название производителя не может быть пустым");
+					errorProvider.SetIconAlignment(newName, ErrorIconAlignment.MiddleRight);
+					args.Cancel = true;
+					return;
+				}
+				var existsProducer = controller.Producers.Where(p => p.Name.ToLower() == newName.Text.Trim() && p.Id != producer.Id).FirstOrDefault();
+				if (existsProducer != null)
+				{
+					errorProvider.SetError(newName, "Такой производитель уже существует");
+					errorProvider.SetIconAlignment(newName, ErrorIconAlignment.MiddleRight);
+					args.Cancel = true;
+					return;
+				}
+				producer.Name = newName.Text;
+				controller.Update(producer);
+			};
 		}
 	}
 }
