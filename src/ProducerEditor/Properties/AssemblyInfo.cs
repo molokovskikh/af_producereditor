468a44505ca02e26e31ca1591a1d98b0e51d979b
Квасов Роман <r.kvasov@analit.net>
2012-03-01 15:42:36 +0400

    исправил сборку AssemblyInfo генерируются автоматически

--- src/ProducerEditor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ProducerEditor")]
-[assembly: AssemblyProduct("ProducerEditor")]
-[assembly: ComVisible(false)]
-[assembly: Guid("e025c5a9-bf68-487f-9f71-c8f246db4961")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
