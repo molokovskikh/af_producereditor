0ff46e697755870d2491e6086a46dc2fbc699590
Квасов Роман <r.kvasov@analit.net>
2012-08-22 10:27:31 +0400

    исправил запуск тестов

--- src/ProducerEditor.Tests/FixtureSetup.cs
+++ src/ProducerEditor.Tests/FixtureSetup.cs
@@ -1,14 +1,36 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using CassiniDev;
+using NUnit.Framework;
 
 namespace ProducerEditor.Tests
 {
 	[SetUpFixture]
 	public class FixtureSetup
 	{
+		private Server webServer;
+
 		[SetUp]
 		public void Setup()
 		{
 			Test.Support.Setup.Initialize();
+			StartServer();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			webServer.Dispose();
+		}
+
+		public void StartServer()
+		{
+			var port = Int32.Parse(ConfigurationManager.AppSettings["webPort"]);
+			var webDir = ConfigurationManager.AppSettings["webDirectory"];
+
+			webServer = new Server(port, "/", Path.GetFullPath(webDir));
+			webServer.Start();
 		}
 	}
 }
